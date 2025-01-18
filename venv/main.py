fc09494e3458b90629ce81eeddc3b83e734905a2
Noragnir <barith9@gmail.com>
2025-01-17 21:47:41 -0600

    screen running and git ignore added

--- venv/main.py
+++ venv/main.py
@@ -7,6 +7,9 @@ def main():
     while True:
         screen.fill("black")
         pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
