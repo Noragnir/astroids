86a22cafb031d25e1571796ffcdf42c8bae3aa01
Noragnir <barith9@gmail.com>
2025-01-18 22:09:33 -0600

    got clock fps clock working

--- venv/main.py
+++ venv/main.py
@@ -3,13 +3,16 @@ from constants import *
 def main():
     print ("Starting asteroids!")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill("black")
         pygame.display.flip()
+        milliseconds = clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = (milliseconds / 1000)
 if __name__ == "__main__":
     main()
