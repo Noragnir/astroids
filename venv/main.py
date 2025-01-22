f4c975b7b80ea3852fa465dd8922782f09618a5e
Noragnir <barith9@gmail.com>
2025-01-21 21:33:46 -0600

    finished chapter and player draw.

--- venv/main.py
+++ venv/main.py
@@ -1,13 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     print ("Starting asteroids!")
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         milliseconds = clock.tick(60)
         for event in pygame.event.get():
