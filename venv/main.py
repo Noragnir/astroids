c08b8811901a9ccedb93aa2a4d603c20d249f66c
Noragnir <barith9@gmail.com>
2025-01-23 15:05:15 -0600

    code update 2

--- venv/main.py
+++ venv/main.py
@@ -10,12 +10,13 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
         milliseconds = clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = (milliseconds / 1000)
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
